f5883edce41d2750a777d1eec1ddb7ee971ed7d8
ashwin212004 <ashwingonbhare@gmail.com>
2025-05-03 08:29:10 +0530

    first commit

---
+++ src/Context/TodoContext.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createContext,useContext } from "react";
+
+export const Todocontext =createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo msg",
+            completed: false,       
+        }
+    ],
+    addTodo:(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deletetodo:(id)=>{},
+    toggleComplete:(id)=>{}
+})
+
+
+export const useTodo=()=>{
+    return useContext(Todocontext)
+}
+
+export const TodoProvider=Todocontext.Provider
